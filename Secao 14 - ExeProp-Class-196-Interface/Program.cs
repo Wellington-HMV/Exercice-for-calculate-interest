bcf701baa99adcaa4de91aff43adfafc1391e53f
Wellington <well.raiser@gmail.com>
2019-07-02 10:18:58 -0300

    Start

---
+++ Secao 14 - ExeProp-Class-196-Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secao_14___ExeProp_Class_196_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
